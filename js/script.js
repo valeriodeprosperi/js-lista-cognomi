65b780a817b3e2ab0e0ffa36737d4cb283f1f070
valerio <valerio.deprosperi@gmail.com>
2020-08-25 15:02:26 +0200

    problema push javascript

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// chieder cognome utente
+var cognomeUtente = prompt("inserisci il tuo cognome");
+// creo array cognomi
+var cognomi = ["rossi","bianchi","ninti","partisi"];
+// inserisco cognomeUtente in array cognomi
+cognomeUtente.push("");
