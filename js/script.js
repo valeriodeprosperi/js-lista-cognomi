225dd81aa5e1abd61193a1488542abfaa15be572
valerio <valerio.deprosperi@gmail.com>
2020-08-26 11:19:14 +0200

    forma contratta inserimento lista ordinata

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ cognomi.sort();
 console.log(cognomi);
 for (var i = 0; i < cognomi.length; i++) {
   console.log(cognomi[i]);
-  var lista = document.getElementById("lista").innerHTML;
-  lista = lista + "<li>" + cognomi[i] + " </li>";
-  document.getElementById("lista").innerHTML = lista;
+  // var lista = document.getElementById("lista").innerHTML;
+  // lista = lista + "<li>" + cognomi[i] + " </li>";
+  document.getElementById("lista").innerHTML += "<li>" + cognomi[i] + " </li>";
 }
