a01dd3f408030dde5c864dff80b7c5339f68c130
valerio <valerio.deprosperi@gmail.com>
2020-08-26 11:54:36 +0200

    posizione cognomeUtente

--- js/script.js
+++ js/script.js
@@ -9,7 +9,16 @@ cognomi.sort();
 console.log(cognomi);
 for (var i = 0; i < cognomi.length; i++) {
   console.log(cognomi[i]);
+  // inserimento cogmoe lista utenti ordinata forma ampia e forma contratta
   // var lista = document.getElementById("lista").innerHTML;
   // lista = lista + "<li>" + cognomi[i] + " </li>";
   document.getElementById("lista").innerHTML += "<li>" + cognomi[i] + " </li>";
 }
+// scrivo la posizione dell'utente nella lista ordinata
+// for (var i = 0; i < cognomi.length; i++) {
+//  if(cognomi[i] == cognomeUtente){
+//    var posizione = i + 1;
+//  }
+// }
+var posizione = cognomi.indexOf(cognomeUtente) + 1;
+document.getElementById("posizione").innerHTML = "la posizione è : " + posizione;
